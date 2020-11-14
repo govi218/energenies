c52a7a7845fe7b66e791caaff3717de32ec0f6f9
bbloks <bmbloks@gmail.com>
2020-11-14 16:52:13 +0100

    added configuration setup and template rendering

--- application/app/__init__.py
+++ application/app/__init__.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+from config import Config
 
 
 def create_app():
 
     app = Flask(__name__)
+    app.config.from_object(Config) 
     
     return app
 
@@ -12,4 +14,4 @@ app = create_app()
 
 @app.route('/')
 def index():
-    return "app"
+    return render_template('index.html')
