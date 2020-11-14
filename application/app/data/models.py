570cd4282fe719c35a946bffbf32db6012995c6b
bbloks <bmbloks@gmail.com>
2020-11-14 21:04:11 +0100

    added devices

--- application/app/data/models.py
+++ application/app/data/models.py
@@ -9,3 +9,10 @@ class User(object):
         return "< User: {} >".format(self.username)
 
 
+class Device(object):
+
+    def __init__(self, name, energy_usage, points, image):
+        self.name = name
+        self.energy_usage = energy_usage
+        self.points = points
+        self.image = image
